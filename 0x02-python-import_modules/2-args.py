391c9412fa3bc355ab75f8afd73f416d0d237842
Badre Ziani <badreziani@gmail.com>
2024-03-14 07:25:23 -0700

    Create task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv[1:]
+    argc = len(argv)
+    if argc <= 0:
+        print("0 arguments.")
+    else:
+        s = "" if argc == 1 else "s"
+        print("{:d} argument{:s}:".format(argc, s))
+        for i, arg in enumerate(argv):
+            print("{:d}: {:s}".format(i + 1, arg))
