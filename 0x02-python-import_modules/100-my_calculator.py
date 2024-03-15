dafb2b9eb2f76413bc36b62d94b2a6bb5a3a1b93
Badre Ziani <badreziani@gmail.com>
2024-03-14 17:20:26 -0700

    Update task 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,21 +5,21 @@ if __name__ == "__main__":
     args = sys.argv
     if len(args) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        sys.exit(1)
     else:
         s = "{:d} {:s} {:d} = {:d}"
         if args[2] == "+":
             print(s.format(int(a), argv[2], int(b), add(int(a), int(b))))
-            exit(0)
+            sys.exit(0)
         elif args[2] == "-":
             print(s.format(int(a), argv[2], int(b), sub(int(a), int(b))))
-            exit(0)
+            sys.exit(0)
         elif args[2] == "*":
             print(s.format(int(a), argv[2], int(b), mul(int(a), int(b))))
-            exit(0)
+            sys.exit(0)
         elif args[2] == "/":
             print(s.format(int(a), argv[2], int(b), div(int(a), int(b))))
-            exit(0)
+            sys.exit(0)
         else:
             print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+            sys.exit(1)
