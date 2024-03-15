b682bdefbfd2320d3f969fee0bf9e06cabf87cdb
Badre Ziani <badreziani@gmail.com>
2024-03-14 17:15:01 -0700

    Create tasl 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,17 +7,18 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     else:
+        s = "{:d} {:s} {:d} = {:d}"
         if args[2] == "+":
-            print("{:d} {:s} {:d} = {:d}".format(int(a), argv[2] , int(b), add(int(a), int(b))))
+            print(s.format(int(a), argv[2], int(b), add(int(a), int(b))))
             exit(0)
         elif args[2] == "-":
-            print("{:d} {:s} {:d} = {:d}".format(int(a), argv[2] , int(b), sub(int(a), int(b))))
+            print(s.format(int(a), argv[2], int(b), sub(int(a), int(b))))
             exit(0)
         elif args[2] == "*":
-            print("{:d} {:s} {:d} = {:d}".format(int(a), argv[2] , int(b), add(int(a), int(b))))
+            print(s.format(int(a), argv[2], int(b), mul(int(a), int(b))))
             exit(0)
         elif args[2] == "/":
-            print("{:d} {:s} {:d} = {:d}".format(int(a), argv[2] , int(b), add(int(a), int(b))))
+            print(s.format(int(a), argv[2], int(b), div(int(a), int(b))))
             exit(0)
         else:
             print("Unknown operator. Available operators: +, -, * and /")
