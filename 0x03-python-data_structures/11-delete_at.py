8eb972350fef9109e3fb8b24b6b15ba92a377b3d
Badre Ziani <badreziani@gmail.com>
2024-03-16 05:33:38 -0700

    Update task 11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,6 +3,6 @@ def delete_at(my_list=[], idx=0):
     """
     Deletes the item at a specific position in a list.
     """
-    if len(my_list) > 0 and 0 <= idx < len(my_list):
-        return [el for el in my_list if my_list.index(el) != idx]
+    if 0 <= idx < len(my_list):
+        return del my_list[idx]
     return my_list
