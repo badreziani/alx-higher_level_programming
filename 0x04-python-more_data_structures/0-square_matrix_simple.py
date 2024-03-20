e4acd05165d228cedb96d766aa3b976223263149
Badre Ziani <badreziani@gmail.com>
2024-03-20 16:05:39 +0000

    Create task 2

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    """
+    Computes the square value of all integers of a matrix.
+    """
     return [[val**2 for val in row] for row in matrix]
