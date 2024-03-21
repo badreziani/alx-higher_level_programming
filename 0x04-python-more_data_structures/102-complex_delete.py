788d8c6d6d3d6b2990ea4edc9c6a2e3947be1ca0
Badre Ziani <badreziani@gmail.com>
2024-03-21 01:43:50 +0000

    Create task 102

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
-
+def complex_delete(a_dictionary, value):
+    keys = [k for k in a_dictionary.keys() if a_dictionary.get(k) is value]
+    for key in keys:
+        del a_dictionary[key]
+    return a_dictionary
