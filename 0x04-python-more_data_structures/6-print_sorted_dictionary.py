8f6670a74be48a35d0d0a6a74a33876e520d2e28
Badre Ziani <badreziani@gmail.com>
2024-03-21 00:00:51 +0000

    Create task 7

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,5 +3,6 @@ def print_sorted_dictionary(a_dictionary):
     """
     Prints a dictionary by ordered keys.
     """
-    for key in sorted(a_dictionary):
-        print(f"{key}: {a_dictionary.get(key)}")
+    if a_dictionary is not None:
+        for key in sorted(a_dictionary):
+            print(f"{key}: {a_dictionary.get(key)}")
