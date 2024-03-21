c07212e92a5eec138ee262850d18a9ca59416a35
Badre Ziani <badreziani@gmail.com>
2024-03-21 00:08:38 +0000

    Update task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -3,5 +3,5 @@ def update_dictionary(a_dictionary, key, value):
     """
     Replaces or adds key/value in a dictionary.
     """
-    if a_dictionary is not None:
-        a_dictionary[key] = value
+    a_dictionary[key] = value
+    return a_dictionary
