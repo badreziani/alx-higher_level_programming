438c2e34f4502d38d7d85eb5dfa705f72c3fb2e8
Badre Ziani <badreziani@gmail.com>
2024-03-21 01:08:33 +0000

    Create task 100

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    """returns the weighted average
+    of all integers tuple (<score>, <weight>)
+    """
+    if not my_list:
+        return 0
+    return sum([x * y for x,y in my_list])/sum([y for x, y in my_list])
