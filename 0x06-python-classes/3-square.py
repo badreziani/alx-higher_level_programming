dd5d728cde128a39f3dff0fab1c0843beacb3665
Badre Ziani <badreziani@gmail.com>
2024-04-02 23:15:48 +0000

    Create Task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+"""This file defines a class 'Square'
+with a private 'size' attribute
+"""
+
+
+class Square:
+    """Square class
+
+    Attributes:
+        size: a private integer attr
+
+    Raises:
+        TypeError: if size is not an integer
+        ValueError: if if size is less than 0
+    """
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """returns the current square area
+        """
+
+        return self.__size ** 2
