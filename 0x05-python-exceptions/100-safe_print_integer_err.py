5e4b4dde2fbbc2e262e0f9106452358dea2973e3
Badre Ziani <badreziani@gmail.com>
2024-04-02 22:18:16 +0000

    Create task 100

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    """prints an integer."""
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError as exc:
+        print("Exception:", exc, file=sys.stderr)
+        return False
