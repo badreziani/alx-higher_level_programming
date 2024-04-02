c2634061f65694f3a3ff14756f4d13ff79717960
Badre Ziani <badreziani@gmail.com>
2024-04-02 16:22:04 +0000

    Update task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,7 +8,6 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[index], end="")
             count += 1
         except Exception:
-            count -= 1
             break
     print()
     return count
