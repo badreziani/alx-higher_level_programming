2039356bed37f6b8c8870601027a90a23fe06278
Badre Ziani <badreziani@gmail.com>
2024-04-02 18:40:01 +0000

    Update Task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     """divides 2 integers and prints the result."""
     try:
-        result = a / b 
+        result = a / b
     except ZeroDivisionError:
         result = None
     finally:
