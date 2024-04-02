1cf6e4b8af908bbd2837a84f0e5b5498ab40c825
Badre Ziani <badreziani@gmail.com>
2024-04-02 23:07:55 +0000

    Create Task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""This file defines a class 'Square'
+with a private 'size' attribute
+"""
+
+
+class Square:
+    """Square class
+
+    Attributes:
+        size: a private integer attr
+    """
+
+    def __init__(self, size=0):
+        self.__size = size
