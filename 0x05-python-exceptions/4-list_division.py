894701b894dac9c6fb08c793d9db7a2a65b6a7de
Badre Ziani <badreziani@gmail.com>
2024-04-02 22:05:17 +0000

    Create task  4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    """divides element by element 2 lists."""
+    results = []
+    for idx in range(list_length):
+        try:
+            result = my_list_1[idx] / my_list_2[idx]
+        except TypeError:
+            result = 0
+            print("wrong type")
+        except ZeroDivisionError:
+            result = 0
+            print("division by 0")
+        except IndexError:
+            result = 0
+            print("out of range")
+        finally:
+            results.append(result)
+    return results
