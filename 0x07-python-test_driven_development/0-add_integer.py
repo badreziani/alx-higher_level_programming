75c54135a761fc7e98954851e6fd5b6e72d028fe
Badre Ziani <badreziani@gmail.com>
2024-04-09 21:55:30 +0000

    Update task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+"""This module contains a definition for
+    add_integer function.
+"""
+
 
 def add_integer(a, b=98):
     """Adds two integers"""
+
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
