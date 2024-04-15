d9fdea96ab25451d9f81261d148607845133afcc
Badre Ziani <badreziani@gmail.com>
2024-04-15 19:58:18 +0100

    Change tests for 7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -20,6 +20,6 @@ class BaseGeometry:
         """Validate an integer"""
 
         if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError("{:s} must be greater than 0".format(name))
