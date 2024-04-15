772409203c4eb2be51f9f2e5351f3e63da4f7d07
Badre Ziani <badreziani@gmail.com>
2024-04-15 14:09:30 +0100

    Task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""This module contains the defintion of
+lookup function.
+"""
+
+
+def lookup(obj):
+    """returns the list of available
+    attributes and methods of an object
+    """
+
+    return dir(obj)
