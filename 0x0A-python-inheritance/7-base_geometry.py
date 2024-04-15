72117c309e7de5519671f8e3b0754c08160bc538
Badre Ziani <badreziani@gmail.com>
2024-04-15 19:48:37 +0100

    Change isinstance to type int Task 7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -20,6 +20,6 @@ class BaseGeometry:
         """Validate an integer"""
 
         if type(value) is not int:
-            raise TypeError("{} must be an integer".format(value))
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(value))
+            raise ValueError("{} must be greater than 0".format(name))
