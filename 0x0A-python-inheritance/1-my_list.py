bac96bc9944c2f08b80e49e22a68ea254b814c97
Badre Ziani <badreziani@gmail.com>
2024-04-15 14:21:23 +0100

    Task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""This module contains the definition
+of class `MyList`
+"""
+
+
+def class MyList(list):
+
+    """Class MyList"""
+
+    def print_sorted(self):
+        """prints the list, but sorted (ascending sort)."""
+
+        sorted_str = str(s).join(", ") for s in sorted(self)
+        print("[{sorted_str}]".format(str))
