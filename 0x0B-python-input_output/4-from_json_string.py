c4e29cd67b5c9ee1e795dcbf8a933058d7357f95
Badre Ziani <badreziani@gmail.com>
2024-04-16 13:42:06 +0100

    Task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+4-from_json_string.py
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    returns an object (Python data structure)
+    represented by a JSON string
+    """
+
+    return json.load(my_str)
