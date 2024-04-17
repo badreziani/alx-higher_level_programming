8b5e4524cde906817c5771487931571a8242ff5c
Badre Ziani <badreziani@gmail.com>
2024-04-17 01:17:15 +0100

    Create task 13

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+100-append_after module
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    inserts a line of text to a file
+    after each line containing a specific string
+    """
+
+    with open(filename) as f:
+        lines = r_file.readlines()
+
+    with open(filename, mode="w") as f:
+        new_text = ""
+        for line in lines:
+            s += line
+            if search_string in line:
+                s += new_string
+        f.write(new_text)
