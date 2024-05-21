c6c64b52d601ca6d3477bfb578328d6e5523d2d5
Badre Ziani <badreziani@gmail.com>
2024-05-21 20:44:10 +0100

    Update task 101

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -2,5 +2,5 @@
 -- ordered by temperature (descending).
 SELECT city, AVG(value) as avg_temp
 FROM temperatures
-GROUP city
+GROUP BY city
 ORDER BY avg_temp DESC;
---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,4 @@
+-- Displays the top 3 of cities temperature
+-- during July and August ordered by temperature (descending)
+SELECT TOP 3, CITY
+FROM 
