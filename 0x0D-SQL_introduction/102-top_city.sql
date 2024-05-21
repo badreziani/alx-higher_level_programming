ec89ab6c9b7c71333ee4769c14369fa1a32d00b2
Badre Ziani <badreziani@gmail.com>
2024-05-21 20:49:17 +0100

    Update task 102

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -2,6 +2,7 @@
 -- during July and August ordered by temperature (descending)
 SELECT city, AVG(value) as avg_temp
 FROM temperatures
+WHERE month in (7, 8)
 GROUP BY city
 ORDER BY avg_temp DESC
 LIMIT 3;
