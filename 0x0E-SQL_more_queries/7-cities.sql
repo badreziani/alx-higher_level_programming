4327026f3d21d98d01274604a15365d1e6dfe5f1
Badre Ziani <badreziani@gmail.com>
2024-05-22 15:55:32 +0100

    Update task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,5 +6,5 @@ CREATE TABLE IF NOT EXISTS cities(
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
 
-	FORIGN KEY (state_id) REFERENCES states(id)
+	FOREIGN KEY (state_id) REFERENCES states(id)
 	);
