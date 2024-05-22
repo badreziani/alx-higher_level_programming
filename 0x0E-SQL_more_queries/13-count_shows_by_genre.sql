a7318126f713258a58eee32ddf887099a5bdc6e1
Badre Ziani <badreziani@gmail.com>
2024-05-22 19:35:03 +0100

    Update task 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,7 +4,5 @@ SELECT tv_genres.name AS 'genre', COUNT(*) AS 'number_of_shows'
 FROM genres
 INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
-INNER JOIN tv_shows
-ON tv_show_genres.show_id = tv_shows.id
-GROUP BY 'genre'
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
