36536ba895828fafa720f42da1a657819937f286
Badre Ziani <badreziani@gmail.com>
2024-06-18 13:54:20 +0100

    Create Task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+if (process.argv.length < 3) {
+  console.log('No argument');
+} else {
+  for (let i = 2; i < process.argv.length; i++) {
+    console.log(process.argv[i]);
+  }
+}
