cd48562865e0ef1dea685ed016d85ed68207640d
Badre Ziani <badreziani@gmail.com>
2024-06-18 19:10:17 +0100

    Create Task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const lines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-let i = 0;
-while (i <= 2) {
+
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < lines.length; i++) {
   console.log(lines[i]);
-  i += 1;
 }
