8706ee9095027c861986c29b9edf04c5be6893ee
Badre Ziani <badreziani@gmail.com>
2024-06-18 18:55:31 +0100

    Create Task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,2 +1,2 @@
 #!/usr/bin/node
-console.log((Number.isNaN(process.argv[2]) || !process.argv[2]) ? 'Not a number' : 'My number: ' + parseInt(process.argv[2]));
+console.log((Number.isNaN(process.argv[2])) ? 'Not a number' : 'My number: ' + parseInt(process.argv[2]));
