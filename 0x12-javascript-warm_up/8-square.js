fd5e1ad21dd13456d341e2042944cdc716321754
Badre Ziani <badreziani@gmail.com>
2024-06-18 19:16:49 +0100

    Create Task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+const size = parseInt(process.argv[2]);
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let x = 0; x < size; x++) {
+    let line = '';
+    for (let y = 0; y < size; y++) {
+      line += 'X';
+    }
+    console.log(line);
+  }
+}
