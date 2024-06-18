c58d9a360a2a169ec44c34b7c35d82db44c9b74c
Badre Ziani <badreziani@gmail.com>
2024-06-18 19:45:56 +0100

    Create Task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+
+if (process.argv.length < 4) {
+  console.log(0);
+} else {
+  const nums = process.argv.slice(2).map(arg => parseInt(arg)).sort();
+  console.log(nums[process.argv.length - 4]);
+}
