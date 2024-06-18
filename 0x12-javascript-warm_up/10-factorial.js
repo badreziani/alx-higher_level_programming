b90c2a41347a14a811b43798d813a3f70fc49ba5
Badre Ziani <badreziani@gmail.com>
2024-06-18 19:28:50 +0100

    Create Task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+const factorial = function (n) {
+  if (isNaN(n) || n === 0) {
+    return 1;
+  }
+  return parseInt(n) * factorial(parseInt(n) - 1);
+};
+
+console.log(factorial(process.argv[2]));
