42394442fbd188ca3348cd875ea60b31351d922b
Badre Ziani <badreziani@gmail.com>
2024-06-19 21:21:46 +0100

    Create Task 12

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -4,13 +4,11 @@ const dict = require('./101-data.js').dict;
 
 const keys = Object.keys(dict);
 
-const newDict = {};
-
-keys.map(key => {
+const newDict = keys.map(function (key) {
   if (newDict[dict[key]] === undefined) {
     newDict[dict[key]] = [];
   }
-  newDict[dict[key]].push(key);
+  return newDict[dict[key]].push(key);
 });
 
 console.log(newDict);
