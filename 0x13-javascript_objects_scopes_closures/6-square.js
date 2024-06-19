7d48b11b400e9c5940b208b196b74d6ff236e37d
Badre Ziani <badreziani@gmail.com>
2024-06-19 13:51:27 +0100

    Create task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+/** class Rectangle **/
+
+const Rectangle = require('./4-rectangle.js');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c !== undefined) {
+      for (let x = 1; x <= this.size; x++) {
+        let line = '';
+        for (let y = 1; y <= this.size; y++) {
+          line += c;
+        }
+        console.log(line);
+      }
+    } else {
+      this.print();
+    }
+  }
+};
