08b2010fce05270258958a04dbb246181c4ce2ea
Badre Ziani <badreziani@gmail.com>
2024-06-19 14:02:22 +0100

    Create task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,14 +1,15 @@
 #!/usr/bin/node
-/** class Rectangle **/
 
-module.exports = class Square extends require('./4-rectangle.js') {
+const OldSquare = require('./5-square.js');
+
+module.exports = class Square extends OldSquare {
   charPrint (c) {
     if (c === undefined) {
       this.print();
     } else {
-      for (let x = 1; x <= this.size; x++) {
+      for (let x = 1; x <= this.height; x++) {
         let line = '';
-        for (let y = 1; y <= this.size; y++) {
+        for (let y = 1; y <= this.width; y++) {
           line += c;
         }
         console.log(line);
