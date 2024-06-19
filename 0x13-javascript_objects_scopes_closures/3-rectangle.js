40d09d7905179dd7e04b3ca4cbc46d7e5e66c629
Badre Ziani <badreziani@gmail.com>
2024-06-19 12:58:47 +0100

    Create task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+/** class Rectangle **/
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let x = 1; x <= this.height; x++) {
+      let line = '';
+      for (let y = 1; y <= this.width; y++) {
+        line += 'X';
+      }
+      console.log(line);
+    }
+  }
+};
