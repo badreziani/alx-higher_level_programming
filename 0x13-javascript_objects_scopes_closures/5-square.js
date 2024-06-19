cdab59f9860d963d874f1b5d91dce1bc4cf0cb06
Badre Ziani <badreziani@gmail.com>
2024-06-19 13:28:17 +0100

    Create task 5

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /** class Rectangle **/
 
-const Rectangle = require('./4-rectangle.js').Rectangle;
+const Rectangle = require('./4-rectangle.js');
 
 module.exports = class Square extends Rectangle {
   constructor (size) {
