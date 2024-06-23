454f47f3f851dfa811c2eeb5abe2532e8e1c792f
Badre Ziani <badreziani@gmail.com>
2024-06-23 01:04:36 +0100

    Creatte task 17

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,2 +1,23 @@
 #!/usr/bin/python3
+"""102-relationship_cities_states_list module
+lists all City objects from the database hbtn_0e_101_usa
+"""
 
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from relationship_state import Base, State
+from relationship_city import City
+
+if __name__ == '__main__':
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                sys.argv[1], sys.argv[2], sys.argv[3]),
+            pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    cities = session.query(City).all()
+    for city in cities:
+        print(f'{state.id}: {state.name} -> {city.state.name}')
