804c0e10422c45f0864b760107a0467f2468887a
Badre Ziani <badreziani@gmail.com>
2024-07-04 21:22:09 +0100

    Task 1, change the shebang

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
-#/usr/bin/bash
+#/bin/bash
 # Takes in a URL, sends a GET request to the URL, and displays the body of the response
 if [ $(curl -s -I $1 | grep 'HTTP/' | awk '{print $2}') == '200' ]; then curl -s -L $1; fi
