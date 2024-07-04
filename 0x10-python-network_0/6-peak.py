98b61ef8d0c6b8f86f7e6efa3fe82758fa89cba8
Badre Ziani <badreziani@gmail.com>
2024-07-04 22:37:57 +0100

    Create files for advanced tasks

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+6-peak module
+"""
+
+def find_peak(list_of_integers):
+    """finds a peak in a list of unsorted integers."""
+    if not list_of_integers:
+        return None
+    
