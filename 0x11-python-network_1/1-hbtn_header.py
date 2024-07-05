0de7b52f84b0b68b03c8be670e6e05553c52b083
Badre Ziani <badreziani@gmail.com>
2024-07-05 11:53:27 +0000

    task 2

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,7 +8,6 @@ variable found in the header of the response
 if __name__ == '__main__':
     import urllib.request
     import sys
-    
+
     with urllib.request.urlopen(sys.argv[1]) as res:
         print(res.headers['X-Request-Id'])
-
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,5 +6,14 @@ and displays the body of the response (decoded in utf-8)
 """
 
 if __name__ == '__main__':
-    import urllib
+    import urllib.request
+    import urllib.parse
     import sys
+
+    values = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('utf-8')
+    req = urllib.request.Request(sys.argv[1])
+    with urllib.request.urlopen(req) as res:
+        res_body = res.read().decode('utf-8')
+        print(f"Your email is: {res_body}")
