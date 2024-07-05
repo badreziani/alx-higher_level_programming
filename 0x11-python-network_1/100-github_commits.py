04fd0f3d679238e112fcb1e8c26f8f897082e994
Badre Ziani <badreziani@gmail.com>
2024-07-05 06:59:08 -0700

    Task 10

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -15,3 +15,12 @@ if __name__ == '__main__':
     import requests
     import sys
 
+    url = f"https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits"
+    headers = {
+            'Accept': 'application/vnd.github+json',
+            'X-GitHub-Api-Version': '2022-11-28'
+            }
+    res = requests.get(url, headers=headers)
+    for commit in res.json()[:10]:
+        print(f"{commit.get('sha')}: ", end="")
+        print(f"{commit.get('commit').get('author').get('name')}")
