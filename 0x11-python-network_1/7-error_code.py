ad3edd469afac53187683637943af0c67f3a8cbb
Badre Ziani <badreziani@gmail.com>
2024-07-05 05:44:40 -0700

    task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,3 +7,9 @@ and displays the body of the response
 if __name__ == '__main__':
     import requests
     import sys
+
+    res = requests.get(sys.argv[1])
+    if res.ok:
+        print(res.text)
+    else:
+        print(f"Error code: {res.status_code}")
