a391b24996b5a6ba9416b81a13b07012ed3780bd
Badre Ziani <badreziani@gmail.com>
2024-07-05 11:22:49 +0000

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,5 +4,12 @@ This script fetches https://alx-intranet.hbtn.io/status
 """
 
 if __name__ == '__main__':
-    import urllib
+    import urllib.request
 
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as res:
+        res_body = res.read()
+        print(f"""Body response:
+            - type: {type(res_body)}
+            - content: {res_body}
+            - utf8 content: {res_body.decode('utf-8')}""")
