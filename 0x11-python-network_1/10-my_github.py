97b6e2e8bee63f1b7cc0fbd27bfa0db204482b2b
Badre Ziani <badreziani@gmail.com>
2024-07-05 06:34:23 -0700

    Task 9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,3 +8,11 @@ if __name__ == '__main__':
     import requests
     import sys
 
+    url = f"https://api.github.com/users/{sys.argv[1]}"
+    headers = {
+            'Accept': 'application/vnd.github+json',
+            'Authorization': f'Bearer {sys.argv[2]}',
+            'X-GitHub-Api-Version': '2022-11-28'
+            }
+    res = requests.get(url, headers=headers)
+    print(res.json().get('id'))
