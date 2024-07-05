cd88ed69549b625c9d0c5752d06d3ea03e7f1ebf
Badre Ziani <badreziani@gmail.com>
2024-07-05 06:10:14 -0700

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,3 +8,15 @@ with the letter as a parameter
 if __name__ == '__main__':
     import requests
     import sys
+
+    data = {"q": sys.argv[1] if len(sys.argv) > 1 else ""}
+    url = "http://0.0.0.0:5000/search_user"
+    res = requests.post(url, data=data)
+    if 'application/json' in res.headers.get('Content-Type'):
+        json_data = res.json()
+        if json_data:
+            print(f"[{json_data['id']}] {json_data['name']}")
+        else:
+            print("No result")
+    else:
+        print("Not a valid JSON")
