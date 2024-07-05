8cad66dd0d6c5f474555b2740a55b0f05dc8dd50
Badre Ziani <badreziani@gmail.com>
2024-07-05 11:30:19 +0000

    update task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as res:
         res_body = res.read()
-        print(f"""Body response:
-            - type: {type(res_body)}
-            - content: {res_body}
-            - utf8 content: {res_body.decode('utf-8')}""")
+        print("Body response:")
+        print(f"\t- type: {type(res_body)}")
+        print(f"\t- content: {res_body}")
+        print(f"\t- utf8 content: {res_body.decode('utf-8')}")
