39167e8e45b33697543e7abfb74017be48e91a8f
Badre Ziani <badreziani@gmail.com>
2024-07-05 05:27:29 -0700

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,3 +5,10 @@ fetches https://alx-intranet.hbtn.io/status
 
 if __name__ == '__main__':
     import requests
+
+    url = "https://alx-intranet.hbtn.io/status"
+    res = requests.get(url)
+    print("Body response:")
+    print(f"\t- type: {type(res.text)}")
+    print(f"\t- content: {res.text}")
+    print(dir(res))
