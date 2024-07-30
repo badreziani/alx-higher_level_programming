0d7b81af4adb7f5217d14a9becfd3fda14a59e32
Badre Ziani <badreziani@gmail.com>
2024-07-30 05:41:22 -0700

    Create task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+const characterId = '/18/';
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(JSON.parse(response.body).results.filter(film => {
+      return film.characters.find(link => link.endsWith(characterId));
+    }).length);
+  }
+});
