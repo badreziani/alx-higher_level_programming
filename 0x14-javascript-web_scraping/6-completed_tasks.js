fcd167d3362628c0434763e0f0a2ad0f3ec279f5
Badre Ziani <badreziani@gmail.com>
2024-07-30 06:39:34 -0700

    Create task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(JSON.parse(response.body).reduce((usersCompletedTasks, todo) => {
+      if (todo.completed) {
+        if (usersCompletedTasks[todo.userId]) {
+          usersCompletedTasks[todo.userId] += 1;
+        } else {
+          usersCompletedTasks[todo.userId] = 1;
+        }
+      }
+      return usersCompletedTasks;
+    }, {}));
+  }
+});
