5e168a2244e2c72629080843f9c4f40f7952d370
Badre Ziani <badreziani@gmail.com>
2024-07-30 04:26:14 -0700

    Create task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const fs = require('node:fs');
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(filePath, content, err => {
+  if (err) {
+    console.log(err);
+  }
+});
