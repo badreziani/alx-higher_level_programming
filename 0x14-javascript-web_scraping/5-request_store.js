bb917504c369bc618169f532a9a830084d846f9b
Badre Ziani <badreziani@gmail.com>
2024-07-30 06:09:48 -0700

    Create task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+
+const request = require('request');
+const fs = require('node:fs');
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    fs.writeFile(filePath, response.body, (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
+});
